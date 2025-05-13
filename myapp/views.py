349f790498e1b0b89b131bda601bc9d881195892
xjackhammered <hammerofjack7@gmail.com>
2025-05-13 23:51:01 +0600

    small changes

--- myapp/views.py
+++ myapp/views.py
@@ -3,6 +3,7 @@ from django.contrib import messages
 from .models import Topic, Task
 from django.contrib.auth.models import User
 from django.contrib.auth import authenticate, login, logout 
+from django.contrib.auth.forms import UserCreationForm
 
 # Create your views here.
 def home(request):
